c69bc22bf460f173a37e50a0c6f8e6c5dab7549f
amart16 <arm6554@psu.edu>
2020-09-04 18:17:24 +0000

    lab1-python

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-celsius=float(input("Enter temperature in celsius: "))
-f=(celsius*1.8)+32
-print(f"{celsius}° in Celsius is equivalent to {f}° Fahrenheit.")
+temp=float(input("Enter temperature: "))
+f=(temp*1.8)+32
+c=(temp-32)/1.8
+
+unit=input("Enter unit in F/f or C/c: ")
+if unit == "F" or unit == "f":
+  print(f"{temp}° in Fahrenheit is equivalent to {c}° Celsius.")
+elif unit == "C" or unit == "c":
+  print(f"{temp}° in Celsius is equivalent to {f}° Fahrenheit.")
+else:
+    print(f"Invalid unit({unit}).")
+  
